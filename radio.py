1f11dfa2947b36cb6f880566350a2021bfdb4e68
Stefan Schroeder <ondekoza@gmail.com>
2022-03-22 21:00:04 +0100

    Rewrite

---
+++ radio.py
@@ -0,0 +1,99 @@
+# Better Raspberry Pi Radio
+# (C) 2017 M4rc3lv
+# Additions and updates by Stefan Schroeder, 2019
+# import RPi.GPIO as GPIO
+import os
+import csv
+import sys
+import time, shlex
+import subprocess
+import curses
+from curses import wrapper
+import json
+
+current_channel = 0
+
+json_string = """
+{ 
+    "radio": [ 
+        "mplayer -playlist http://www.ndr.de/resources/metadaten/audio/m3u/ndrinfo.m3u", 
+        "mplayer https://st01.dlf.de/dlf/01/128/mp3/stream.mp3"
+    ],
+    "amixer": "Master",
+    "current": 0,
+    "volume": 60
+} 
+"""
+data = json.loads(json_string)
+
+# Clamp value btw 0 and 100
+clamp = lambda n: max(min(100, n), 0)
+
+def shutdownRadio(dummy):
+    print("Shutdown")
+#    os.system("sudo halt")
+
+def VolUp():
+    applyVolume(data["volume"] + 4)
+
+def VolDown():
+    applyVolume(data["volume"] - 4)
+
+def applyVolume(volume):
+    data["volume"] = clamp(volume)
+    subprocess.Popen(shlex.split("amixer sset '"+data["amixer"]+"' "+str(data["volume"])+"%"))
+
+def SetRadioChannel(chan):
+    os.system("pkill mplayer");
+    chan = data["current"]
+    args = shlex.split(data["radio"][chan]+" &")
+    my_env = os.environ.copy()
+    my_env["MPLAYER_VERBOSE"] = "-1"
+    subprocess.Popen(args, env=my_env)
+
+def SetRadioChannelIndex(c):
+    data["current"] = c
+    SetRadioChannel()
+
+def SetRadioChannelUp():
+    data["current"] = ( data["current"] + 1 ) % len(data["radio"])
+    SetRadioChannel()
+    
+def SetRadioChannelDown():
+    current_channel = ( current_channel - 1 + len(data["radio"])) % len(data["radio"])
+    SetRadioChannel()
+
+def cleanup():
+    os.system("pkill mplayer");
+    curses.nocbreak()
+    # screen.keypad(0)
+    curses.echo()
+    curses.endwin()
+    sys.exit()
+
+def main():
+    SetRadioChannel(data["current"])
+
+    damn = curses.initscr()
+    damn.nodelay(1)
+    while True:
+        c = damn.getch()
+        if c > 1:
+            print("<" + str(c) + ">")
+        if c == 122: # 'z'
+            cleanup()
+        if c == 109: # 'm'
+            SetRadioChannelUp()
+        if c == 110: # 'n'
+            SetRadioChannelDown()
+        if c == 120: # 'x'
+            VolUp()
+        if c == 121: # 'y'
+            VolDown()
+        if c == 48: # '0'
+            SetRadioChannelIndex(0)
+        if c == 49: # '1'
+            SetRadioChannelIndex(1)
+
+main()
+
--- rpi-radio.py
+++ rpi-radio.py
@@ -1,7 +1,7 @@
 # Better Raspberry Pi Radio
 # (C) 2017 M4rc3lv
 # Additions and updates by Stefan Schroeder, 2019
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import os
 import csv
 import sys
@@ -10,7 +10,7 @@ import subprocess
 import curses
 from curses import wrapper
 
-GPIO.setmode(GPIO.BOARD)
+# GPIO.setmode(GPIO.BOARD)
 CurChannel = 0
 current_channel = 0
 ChannelURL = []
@@ -20,8 +20,7 @@ Last = ""
 with open('radio.csv') as csvfile:
     channelreader = csv.reader(csvfile)
     for row in channelreader:
-	if row[0].startswith("#"):
-		continue 
+        if row[0].startswith("#"): continue 
         ChannelName.append(row[0])
         ChannelURL.append(row[1])
 
@@ -40,14 +39,15 @@ volume = 50
 Prev=0
 
 def shutdownRadio(dummy):
-	print("Shutdown")
-	os.system("sudo halt")
+    print("Shutdown")
+    os.system("sudo halt")
 
 def Rot(channel):
     global Last
     global Prev
     
-    level = GPIO.input(channel)
+    #level = GPIO.input(channel)
+    level= 5
     Last = str(channel)+":"+str(level)
     if Last=="5:0" and Prev=="7:0": VolUp(1)
     if Last=="7:0" and Prev=="5:0": VolDown(1)
@@ -63,20 +63,20 @@ def VolUp(dummy):
     file.close()
 
 def VolDown(dummy):
-	global volume
-	volume = volume - 4
-	if volume<=0: volume = 0    
-	subprocess.Popen(shlex.split("amixer sset 'PCM' "+str(volume)+"%"))
-	file = open("lastvolume.txt","w")
-	file.write(str(volume)+"\n")
-	file.close()
+    global volume
+    volume = volume - 4
+    if volume<=0: volume = 0    
+    subprocess.Popen(shlex.split("amixer sset 'PCM' "+str(volume)+"%"))
+    file = open("lastvolume.txt","w")
+    file.write(str(volume)+"\n")
+    file.close()
 
 def SetRadioChannel(chan):
     os.system("pkill mplayer");
     os.system('echo "'+ChannelName[chan]+'" | festival --tts')
     args = shlex.split("mplayer "+ChannelURL[chan]+" &")
-	my_env = os.environ.copy()
-	my_env["MPLAYER_VERBOSE"] = "-1"
+    my_env = os.environ.copy()
+    my_env["MPLAYER_VERBOSE"] = "-1"
     subprocess.Popen(args, env=my_env)
 
 def SetRadioChannelIndex(c):
@@ -105,64 +105,62 @@ def cleanup():
     sys.exit()
 
 def RotButton(dummy):
-	SetRadioChannelUp()
+    SetRadioChannelUp()
 
 def main():
-	try:
-	    try:
-		# Program start: restore volume
-		try:
-		    file = open("lastvolume.txt","r")
-		    volume = int(file.read())
-		    subprocess.Popen(shlex.split("amixer sset 'PCM' "+str(volume)+"%"))
-		    file.close()
-		except: pass
+    try:
+        try:
+            file = open("lastvolume.txt","r")
+            volume = int(file.read())
+            subprocess.Popen(shlex.split("amixer sset 'PCM' "+str(volume)+"%"))
+            file.close()
+        except: pass
 
-		file = open("LastChannel.txt","r")
-		CurChannel = int(file.read())
-		file.close()
-	    except:        
-		CurChannel=0
+        file = open("LastChannel.txt","r")
+        CurChannel = int(file.read())
+        file.close()
+    except:        
+        CurChannel=0
 
-	    SetRadioChannel(0)
+        SetRadioChannel(0)
 
-	    GPIO.setup(VOL_UP, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	    GPIO.add_event_detect(VOL_UP, GPIO.RISING, callback=VolUp, bouncetime=150)
+        GPIO.setup(VOL_UP, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(VOL_UP, GPIO.RISING, callback=VolUp, bouncetime=150)
 
-	    GPIO.setup(PIN_VOL_DOWN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	    GPIO.add_event_detect(PIN_VOL_DOWN, GPIO.RISING, callback=VolDown, bouncetime=150)
-	    
-	    GPIO.setup(PIN_A, GPIO.IN) # pull_up_down=GPIO.PUD_UP)
-	    GPIO.setup(PIN_B, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	    GPIO.add_event_detect(PIN_A, GPIO.FALLING, callback=Rot)
-	    GPIO.add_event_detect(PIN_B, GPIO.FALLING, callback=Rot)	    
+        GPIO.setup(PIN_VOL_DOWN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(PIN_VOL_DOWN, GPIO.RISING, callback=VolDown, bouncetime=150)
+        
+        GPIO.setup(PIN_A, GPIO.IN) # pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(PIN_B, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(PIN_A, GPIO.FALLING, callback=Rot)
+        GPIO.add_event_detect(PIN_B, GPIO.FALLING, callback=Rot)        
 
-	    GPIO.setup(PIN_SHUTOWN_SWITCH, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	    GPIO.add_event_detect(PIN_SHUTOWN_SWITCH, GPIO.FALLING, callback=shutdownRadio, bouncetime=200)
+        GPIO.setup(PIN_SHUTOWN_SWITCH, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(PIN_SHUTOWN_SWITCH, GPIO.FALLING, callback=shutdownRadio, bouncetime=200)
 
-	    GPIO.setup(PIN_ROTARY_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	    GPIO.add_event_detect(PIN_ROTARY_BUTTON, GPIO.FALLING, callback=RotButton, bouncetime=250)
+        GPIO.setup(PIN_ROTARY_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(PIN_ROTARY_BUTTON, GPIO.FALLING, callback=RotButton, bouncetime=250)
 
 
-	    damn = curses.initscr()
-	    damn.nodelay(1)
-	    while True:
-		c = damn.getch()
-		if c > 1:
-		    print("<" + str(c) + ">")
-		if c == 122: # 'z'
-		    cleanup()
-		if c == 109: # 'm'
-		    SetRadioChannelUp()
-		if c == 110: # 'n'
-		    SetRadioChannelDown()
-		if c == 120: # 'x'
-		    VolUp(1)
-		if c == 121: # 'y'
-		    VolDown(1)
-		if c == 49: # '1'
-		    SetRadioChannelIndex(1)
-	finally:
-	    GPIO.cleanup()        
+        damn = curses.initscr()
+        damn.nodelay(1)
+        while True:
+            c = damn.getch()
+            if c > 1:
+                print("<" + str(c) + ">")
+            if c == 122: # 'z'
+                cleanup()
+            if c == 109: # 'm'
+                SetRadioChannelUp()
+            if c == 110: # 'n'
+                SetRadioChannelDown()
+            if c == 120: # 'x'
+                VolUp(1)
+            if c == 121: # 'y'
+                VolDown(1)
+            if c == 49: # '1'
+                SetRadioChannelIndex(1)
+    finally:
+        GPIO.cleanup()        
 
 wrapper(main())
